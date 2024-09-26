319e04fc113939f1fbd006d3694779068d9fd6ba
redx17775 <65367038+redx17775@users.noreply.github.com>
2024-09-26 15:11:15 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const myH1_1 = document.getElementById("myH1_1");
+const myH1_2 = document.getElementById("myH1_2");
+
+const cake = document.getElementById("cake1");
+
+const myP_1 = document.getElementById("myP_1");
+const myP_2 = document.getElementById("myP_2");
+
+const audio = new Audio("mp3/Yippee sound effect.mp3");
+
+cake.addEventListener("click", (event) => {
+  event.target.src = "img/cake2.gif";
+  setTimeout(afterTheCake, 1500);
+});
+
+function afterTheCake() {
+  myH1_1.style.display = "none";
+  myH1_2.style.display = "block";
+  cake.src = "img/kitty.gif";
+  cake.style.width = "50%";
+  myP_1.style.display = "none";
+  myP_2.style.display = "block";
+  audio.play();
+}
