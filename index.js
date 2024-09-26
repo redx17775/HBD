4c2b26f3f1825268b9fb0123dd135972d081128d
redx17775 <65367038+redx17775@users.noreply.github.com>
2024-09-26 15:44:43 +0400

    update style.css and fix kitty.gif and sound sync

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const myH1_1 = document.getElementById("myH1_1");
 const myH1_2 = document.getElementById("myH1_2");
 
 const cake = document.getElementById("cake1");
-const kitty = document.getElementById("kitty");
+const div = document.getElementById("cake_div");
 
 const myP_1 = document.getElementById("myP_1");
 const myP_2 = document.getElementById("myP_2");
@@ -18,8 +18,10 @@ function afterTheCake() {
   myH1_1.style.display = "none";
   myH1_2.style.display = "block";
   cake.style.display = "none";
-  kitty.style.display = "block";
-  kitty.style.width = "50%";
+  const kitty = document.createElement("img");
+  div.append(kitty);
+  kitty.style.width = "40%";
+  kitty.src = "img/kitty.gif";
   myP_1.style.display = "none";
   myP_2.style.display = "block";
   audio.play();
