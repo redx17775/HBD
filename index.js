8e4d8834451102f0ba7f0e35d40c162d9d12b2ea
redx17775 <65367038+redx17775@users.noreply.github.com>
2024-09-26 15:29:47 +0400

    fix index.js and update style.css
    index.js: fix the eventListener to work once by adding kitty to new img in html.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const myH1_1 = document.getElementById("myH1_1");
 const myH1_2 = document.getElementById("myH1_2");
 
 const cake = document.getElementById("cake1");
+const kitty = document.getElementById("kitty");
 
 const myP_1 = document.getElementById("myP_1");
 const myP_2 = document.getElementById("myP_2");
@@ -16,8 +17,9 @@ cake.addEventListener("click", (event) => {
 function afterTheCake() {
   myH1_1.style.display = "none";
   myH1_2.style.display = "block";
-  cake.src = "img/kitty.gif";
-  cake.style.width = "50%";
+  cake.style.display = "none";
+  kitty.style.display = "block";
+  kitty.style.width = "50%";
   myP_1.style.display = "none";
   myP_2.style.display = "block";
   audio.play();
